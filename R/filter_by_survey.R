34b40eae4c79a897a1970707f2f6ad79143737b2
ERIC North East <eric.ne@twmuseums.org.uk>
2022-04-05 15:19:40 +0100

    Remove Cumbria BDC records

--- R/filter_by_survey.R
+++ R/filter_by_survey.R
@@ -33,7 +33,9 @@
 #' data <- filter_by_survey(df)
 filter_by_survey <- function(raw_data){
   raw_data <- dplyr::filter(raw_data,!(raw_data$Survey.Run=='Northumbrian Water' & raw_data$Taxon.Comm=='Violet Crystalwort'))
+  raw_data <- dplyr::filter(raw_data,!(raw_data$Survey.Run=='Cumbria Biodiversity Data Centre'))
   filtered_data <- dplyr::filter(raw_data,!(raw_data$Survey.Run=='Tom Tams' & raw_data$Taxon.Comm=='Bedstraw Hawk-moth' & ((stringr::str_length(raw_data$Sample.Spa)==5 & stringr::str_sub(raw_data$Sample.Spa,end=4)=='NU14') | (stringr::str_c(stringr::str_sub(raw_data$Sample.Spa,end=3),stringr::str_sub(raw_data$Sample.Spa,start=(stringr::str_length(raw_data$Sample.Spa)+4)/2,end=(stringr::str_length(raw_data$Sample.Spa)+4)/2))=='NU14'))))
+
 
   return(filtered_data)
 }
