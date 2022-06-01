edcd8b395b4c6391ee76567db27dcb8fd0af8a95
ERIC North East <eric.ne@twmuseums.org.uk>
2022-06-01 09:38:14 +0100

    Recorder not needed

--- R/setup_EA_SLA_config_values.R
+++ R/setup_EA_SLA_config_values.R
@@ -5,7 +5,7 @@
 #'
 #' @examples config <- setup_EA_SLA_config_values()
 setup_EA_SLA_config_values <- function(){
-  config_keys <- c("locationCol", "abundanceCol", "commentCol", "recorderCol", "lastCol", "dateCol", "EAOutputCols", "TempColNames")
+  config_keys <- c("locationCol", "abundanceCol", "commentCol", "lastCol", "dateCol", "EAOutputCols", "TempColNames")
 
 
   locationCol <- 4
@@ -20,7 +20,7 @@ setup_EA_SLA_config_values <- function(){
   TempColNames <- c("Common_nam","Scientific","Abundances","Location Name","Survey_sta","Survey_end","Survey_typ","File_code","Data_provi","Data_owner","Supplier","Source","Capture_GR","Data_restr","Status","Comments","OSGB_E","OSGB_N")
 
 
-  config_values <- list(locationCol, abundanceCol, commentCol, recorderCol, lastCol, dateCol,   EAOutputCols, TempColNames)
+  config_values <- list(locationCol, abundanceCol, commentCol, lastCol, dateCol,   EAOutputCols, TempColNames)
 
   config_pairs <- list()                     # Create empty list
   for(i in 1:length(config_keys)) {              # Add key/value pairs in for-loop
