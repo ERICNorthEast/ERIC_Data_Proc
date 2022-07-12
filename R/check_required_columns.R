28029010c4ee7873846ad0c39d48e3220d66ce62
ERIC North East <eric.ne@twmuseums.org.uk>
2022-07-12 15:54:23 +0100

    Added examples for documentation

--- R/check_required_columns.R
+++ R/check_required_columns.R
@@ -6,6 +6,21 @@
 #' @export
 #'
 #' @examples
+
+
+#'sn <- c('Pieris rapae','Turdus merula')
+#'cn <- c('Small White','Blackbird')
+#'rec <- c('Mike Jeffries','Alnwick Wildlife Group')
+#'loc <- c('Morpeth Town','Morpeth Town')
+#'dt <- c('04/09/2010','04/09/2010')
+#'gr <- c('NZ18X','NZ18X')
+#'com <- c('Butterfly Conservation','Alnwick Wildlife Group')
+
+#'ab <- c('2 Count','1 Count')
+
+#'data <- data.frame(rec,cn,sn,dt,gr,loc,ab,com)
+#'colmames(data) <- c("Recorder","Common Name","Species Name", "Date", "Grid Reference", "Location Name", "Abundances","Comments")
+#'   result <- check_required_columns(data,'std')
 check_required_columns <- function(data_set,data_format) {
 
   # if (data_format == "std") {
