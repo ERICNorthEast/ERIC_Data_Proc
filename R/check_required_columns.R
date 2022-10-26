9a013537095a883f5ee82370e6785c2767d14c81
ERIC North East <eric.ne@twmuseums.org.uk>
2022-10-26 15:03:25 +0100

    Pinpoint has been removed from BirdTrack downloads

--- R/check_required_columns.R
+++ R/check_required_columns.R
@@ -30,7 +30,7 @@ check_required_columns <- function(data_set,data_format) {
     "std" = return (all(c("Recorder","Common Name","Species Name", "Date", "Grid Reference", "Location Name", "Abundances","Comments") %in% colnames(data_set))),
     "irecord" = return (all(c("Recorder","Common Name","Species Name", "Date", "Grid Reference", "Location Name", "Abundances","Comments","Sample comment","Biotope","Sample method","Sex","Stage","Verifier") %in% colnames(data_set))),
     "eric" = return (all(c("Recorder","Common Name","Species Name", "Date", "Grid Reference", "Location Name", "Abundances","Comments","Sample comment","Biotope","Sample method","Sex","Stage","Verifier") %in% colnames(data_set))),
-    "bt" = return (all(c("Recorder","Common Name","Species Name", "Date", "Grid Reference", "Location Name", "Abundances","Comments", "Breeding status","Breeding details","Pinpoint","Plumage") %in% colnames(data_set)))
+    "bt" = return (all(c("Recorder","Common Name","Species Name", "Date", "Grid Reference", "Location Name", "Abundances","Comments", "Breeding status","Breeding details","Plumage") %in% colnames(data_set)))
 
   )
 }
