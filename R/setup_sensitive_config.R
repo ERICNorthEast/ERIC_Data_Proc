5a38eec7f4386f093e210e94515db904be2331e9
ERIC North East <eric.ne@twmuseums.org.uk>
2022-12-06 08:03:24 +0000

    Myotis & chiroptera were missing from the species to flag

--- R/setup_sensitive_config.R
+++ R/setup_sensitive_config.R
@@ -8,7 +8,7 @@ setup_sensitive_config <- function(){
 
   sens_config <- list()
 
-  sens_config$sensitive_species <- c("Falco peregrinus" ,"Accipiter gentilis" ," Circus cyaneus" ," Circus aeruginosus" ," Aquila chrysaetos","Balaenoptera acutorostrata" ,"Lagenorhynchus albirostris" ,"Lutra lutra" ,"Martes martes" ,"Meles meles" ,"Pipistrellus" ,"Vespertilionidae", "Corallorhiza trifida" ,"Epipactis dunensis" ,"Epipactis phyllanthes" ,"Epipactis sancta" ,"Epipactis youngiana","Margaritifera margaritifera" ,"Hyles gallii" ,"Austropotamobius pallipes")
+  sens_config$sensitive_species <- c("Falco peregrinus" ,"Accipiter gentilis" ," Circus cyaneus" ," Circus aeruginosus" ,"Aquila chrysaetos","Balaenoptera acutorostrata" ,"Lagenorhynchus albirostris" ,"Lutra lutra" ,"Martes martes" ,"Meles meles" ,"Pipistrellus" ,"Vespertilionidae", "Corallorhiza trifida" ,"Epipactis dunensis" ,"Epipactis phyllanthes" ,"Epipactis sancta" ,"Epipactis youngiana","Margaritifera margaritifera" ,"Hyles gallii" ,"Austropotamobius pallipes","Myotis","Chiroptera")
   sens_config$sensitive_desigs <- c("WACA-Sch5_sect9.1\\(kill/injuring\\)", "WACA-Sch1_part1", "WACA-Sch8")
 
   return(sens_config)
