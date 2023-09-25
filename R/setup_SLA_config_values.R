1765d26fc29c7a05e30dfc3bfe66718f6e517dad
ERIC North East <93654712+ERICNorthEast@users.noreply.github.com>
2023-09-25 15:52:27 +0100

    Update setup_SLA_config_values.R
    Keep the Obs Key column in the output file

--- R/setup_SLA_config_values.R
+++ R/setup_SLA_config_values.R
@@ -13,7 +13,7 @@ setup_SLA_config_values <- function(){
   abundanceCol <- 8
   commentCol <- 10
   recorderCol <- 11
-  lastCol <- 24
+  lastCol <- 25
   dateCol <- 13
 
   #LA cols
@@ -23,8 +23,8 @@ setup_SLA_config_values <- function(){
   LArecorderCol <- 10
   LAlastCol <- 26
 
-  SLAOutputCols <- c("All.Design","AllUKLeg", "Wildlife..","Wildlife_1","broadgroup", "Taxon.grou", "Taxon.Lati","Obs.Abunda", "Determinat",              "Obs.Commen","Sample.Rec","Sample.Loc",  "Sample.Dat","Sample.Spa",    "Central_eastings","Central_northings","Buffer","Precision","Survey.Run","Survey.Nam","NELBAP","Taxon.Comm","ImportDate","Info")
-  SLAColNames <- c("Designations","All UK Legally Protected (W&C Act, Badgers and Cons Regs) - Taxon Designated","Wildlife & Countryside Act Sch 9 Part 2","Wildlife & Countryside Act Sch 9 Part 1","Broad Group","Taxon group","Latin Name","Abundances","Determination Type","Comments","Recorder","Location Name","Date",  "Grid Reference","Cent_East","Cent_North","Buffer","Precision",             "Survey Run By","Survey Name",                     "North East LBAP - Short Names","Common Name","Obs Entry Date","Additional Information") # Three empty cols
+  SLAOutputCols <- c("All.Design","AllUKLeg", "Wildlife..","Wildlife_1","broadgroup", "Taxon.grou", "Taxon.Lati","Obs.Abunda", "Determinat",              "Obs.Commen","Sample.Rec","Sample.Loc",  "Sample.Dat","Sample.Spa",    "Central_eastings","Central_northings","Buffer","Precision","Survey.Run","Survey.Nam","NELBAP","Taxon.Comm","ImportDate","Obs.Key","Info")
+  SLAColNames <- c("Designations","All UK Legally Protected (W&C Act, Badgers and Cons Regs) - Taxon Designated","Wildlife & Countryside Act Sch 9 Part 2","Wildlife & Countryside Act Sch 9 Part 1","Broad Group","Taxon group","Latin Name","Abundances","Determination Type","Comments","Recorder","Location Name","Date",  "Grid Reference","Cent_East","Cent_North","Buffer","Precision",             "Survey Run By","Survey Name",                     "North East LBAP - Short Names","Common Name","Obs Entry Date","Obs Key","Additional Information") # Three empty cols
   LAOutputCols <- c("All.Design","AllUKLeg", "Wildlife..","Wildlife_1", "Taxon.grou", "Taxon.Lati","Obs.Abunda", "Determinat",   "Obs.Commen","Sample.Rec","Sample.Loc",  "Sample.Dat","Sample.Spa",    "Central_eastings","Central_northings","Buffer","Precision","Survey.Run","Survey.Nam","NELBAP","Taxon.Comm","Empty1","Empty2","ImportDate","Empty3","Info")
   LAColNames <- c("Designations","All UK Legally Protected (W&C Act, Badgers and Cons Regs) - Taxon Designated","Wildlife & Countryside Act Sch 9 Part 2","Wildlife & Countryside Act Sch 9 Part 1","Taxon group","Latin Name","Abundances","Determination Type","Comments","Recorder","Location Name","Date",  "Grid Reference","Cent_East","Cent_North","Buffer","Precision",  "Survey Run By","Survey Name",          "North East LBAP - Short Names","Common Name","England NERC S.41","Obs Changed Date","Obs Entry Date","Obs Key","Additional Information") # Three empty cols
 
